e15d5eec02f3ace79af5b5cc2d07aeb0000b7a76
GuillaumeLiard <liardg@gmail.com>
2022-12-03 16:51:18 +0100

    generate sound

---
+++ components/GenerateSound/index.tsx
@@ -0,0 +1,27 @@
+export default function GenerateSound() {
+    const start = () => alert('start')
+    const stop = () => alert('stop')
+    return (
+        <section>
+            <div className="flex flex-col items-center px-5 py-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="flex flex-col w-full max-w-3xl mx-auto prose text-left prose-blue">
+                    <div className="w-full mx-auto">
+                        <h2>GenerateSound</h2>
+                        <div className="flex flex-wrap justify-start flex-grow mt-8 text-left md:mt-0">
+                            <div className="w-full space-y-4 md:w-1/2">
+                                <button onClick={start}>
+                                    start
+                                </button>
+                            </div>
+                            <div className="w-full space-y-4 md:w-1/2">
+                                <button onClick={stop}>
+                                    stop
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import GenerateSound from '../components/GenerateSound'
+
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-5xl font-bold underline">
+        Propriocepson
+      </h1>
+      <GenerateSound/>
+    </div>
   )
 }
