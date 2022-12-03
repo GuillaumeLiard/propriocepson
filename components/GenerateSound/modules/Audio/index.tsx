9b84114cb4e4036c0977d3611a9a759838f9e9e0
GuillaumeLiard <liardg@gmail.com>
2022-12-03 18:02:18 +0100

    frequency period 10

--- components/GenerateSound/modules/Audio/index.tsx
+++ components/GenerateSound/modules/Audio/index.tsx
@@ -1,18 +1,45 @@
+const HIGH_VOLUME = 0.1
+const ZERO_VOLUME = 0
+const LOW_FREQUENCY = 100
+const HIGH_FREQUENCY = 440
+const PERIOD = 10
+
+import gsap from 'gsap'
+
 export default class SingletonAudio {
     private static instance: SingletonAudio;
     gainNode: GainNode
+    oscillatorNode: OscillatorNode
+    context: AudioContext
 
     private constructor() {
-        let oscillatiorNode: OscillatorNode
-        let context = new AudioContext()
-        oscillatiorNode = context.createOscillator()
-        oscillatiorNode.type = "sine"
-        oscillatiorNode.start()
+        this.context = new AudioContext()
+        this.oscillatorNode = this.context.createOscillator()
+        this.configOscillator()
+        this.gainNode = this.context.createGain()
+        this.oscillatorNode.connect(this.gainNode)
+        this.gainNode.connect(this.context.destination)
+    }
 
-        this.gainNode = context.createGain()
+    configOscillator() {
+        this.oscillatorNode = this.context.createOscillator()
+        this.oscillatorNode.type = "sine"
+        this.oscillatorNode.start()
+        const frequency = {
+            value: LOW_FREQUENCY
+        }
+        const timeline = gsap.timeline({ repeat: -1, yoyo: true })
+        const up = gsap.to(frequency, {
+            value: HIGH_FREQUENCY,
+            duration: PERIOD / 2,
+            onUpdate: () => {
+                console.log('frequency.value', frequency.value)
+                // this.oscillatorNode.setPeriodicWave
+                this.oscillatorNode.frequency.setValueAtTime(frequency.value, this.context.currentTime); // value in hertz
 
-        oscillatiorNode.connect(this.gainNode)
-        this.gainNode.connect(context.destination)
+            }
+        })
+        timeline.add(up)
     }
 
     public static getInstance(): SingletonAudio {
@@ -24,9 +51,9 @@ export default class SingletonAudio {
     }
 
     start() {
-        this.gainNode.gain.setValueAtTime(0.2, 0)
+        this.gainNode.gain.setValueAtTime(HIGH_VOLUME, this.context.currentTime)
     }
     stop() {
-        this.gainNode.gain.setValueAtTime(0, 0)
+        this.gainNode.gain.setValueAtTime(ZERO_VOLUME, this.context.currentTime)
     }
 }
