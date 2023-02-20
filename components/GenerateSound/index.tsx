b9a0e6a0a043481a915adf866ab3c94f7227a322
GuillaumeLiard <liardg@gmail.com>
2023-02-20 20:30:29 +0100

    remove tailwind, use css modules

--- components/GenerateSound/index.tsx
+++ components/GenerateSound/index.tsx
@@ -1,44 +1,31 @@
 import { start, stop } from './controller'
+import styles from './index.module.scss'
 const VERSION = 6
 export default function GenerateSound() {
 
     return (
-        <section>
-            <div className="flex flex-col items-center px-5 py-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div className="flex flex-col w-full max-w-3xl mx-auto prose text-left prose-blue">
-                    <div className="w-full mx-auto">
-                        <h2>GenerateSound</h2>
-                        <p>version : {VERSION}</p>
-                        <div className="flex flex-wrap justify-start flex-grow mt-8 text-left md:mt-0">
-                            <div className="w-full space-y-4 md:w-1/2">
-                                <button onClick={() => start()}>
-                                    start
-                                </button>
-                            </div>
-                            <div className="w-full space-y-4 md:w-1/2">
-                                <button onClick={() => start(2)}>
-                                    start 2x
-                                </button>
-                            </div>
-                            <div className="w-full space-y-4 md:w-1/2">
-                                <button onClick={() => start(3)}>
-                                    start 3x
-                                </button>
-                            </div>
-                            <div className="w-full space-y-4 md:w-1/2">
-                                <button onClick={() => start(4)}>
-                                    start 4x
-                                </button>
-                            </div>
-                            <div className="w-full space-y-4 md:w-1/2">
-                                <button onClick={stop}>
-                                    stop
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className={styles.main}>
+            <div className="header">
+                <h2>GenerateSound</h2>
+                <p>version : {VERSION}</p>
             </div>
-        </section>
+            <div className={styles.inputs}>
+                <button className={styles.button} onClick={() => start()}>
+                    start
+                </button>
+                <button className={styles.button} onClick={() => start(2)}>
+                    start 2x
+                </button>
+                <button className={styles.button} onClick={() => start(3)}>
+                    start 3x
+                </button>
+                <button className={styles.button} onClick={() => start(4)}>
+                    start 4x
+                </button>
+                <button className={styles.button} onClick={stop}>
+                    stop
+                </button>
+            </div>
+        </div>
     )
 }
