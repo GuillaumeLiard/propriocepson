f032aae97932a844ba202d4cf9c126292d212d97
GuillaumeLiard <liardg@gmail.com>
2023-03-09 01:23:27 +0100

    update style

--- components/GenerateSound/index.tsx
+++ components/GenerateSound/index.tsx
@@ -9,44 +9,41 @@ export default function GenerateSound() {
     } = usePlayer()
     return (
         <div className={styles.main}>
-            <div className="header">
-                <h2>GenerateSound</h2>
-                <p>version : {VERSION}</p>
-                <p>duration : {duration}</p>
+            <div className={styles.top}>
+                <div className="header">
+                    <h2>GenerateSound</h2>
+                    <p>version : {VERSION}</p>
+                    <p>duration : {duration}</p>
+                </div>
+                <button className={styles.button} onClick={stop}>
+                    stop
+                </button>
             </div>
-            <div className={styles.container}>
-                <div className={styles.inputs}>
-                    <button className={styles.button} onClick={() => play(5)}>
-                        play wave (5s)
-                    </button>
-                    <button className={styles.button} onClick={() => play(6)}>
-                        play wave (6s)
-                    </button>
-                    <button className={styles.button} onClick={() => play(7)}>
-                        play wave (7s)
-                    </button>
-                    <button className={styles.button} onClick={() => play(8)}>
-                        play wave (8s)
-                    </button>
-                </div><div className={styles.inputs}>
-                    <button className={styles.button} onClick={() => play(9)}>
-                        play wave (9s)
-                    </button>
-                    <button className={styles.button} onClick={() => play(10)}>
-                        play wave (10s)
-                    </button>
-                    <button className={styles.button} onClick={() => play(11)}>
-                        play wave (11s)
-                    </button>
-                    <button className={styles.button} onClick={() => play(12)}>
-                        play wave (12s)
-                    </button>
-                </div>
-                <div className={styles.inputs}>
-                    <button className={styles.button} onClick={stop}>
-                        stop
-                    </button>
-                </div>
+            <div className={styles.inputs}>
+                <button className={styles.button} onClick={() => play(5)}>
+                    play wave (5s)
+                </button>
+                <button className={styles.button} onClick={() => play(6)}>
+                    play wave (6s)
+                </button>
+                <button className={styles.button} onClick={() => play(7)}>
+                    play wave (7s)
+                </button>
+                <button className={styles.button} onClick={() => play(8)}>
+                    play wave (8s)
+                </button>
+                <button className={styles.button} onClick={() => play(9)}>
+                    play wave (9s)
+                </button>
+                <button className={styles.button} onClick={() => play(10)}>
+                    play wave (10s)
+                </button>
+                <button className={styles.button} onClick={() => play(11)}>
+                    play wave (11s)
+                </button>
+                <button className={styles.button} onClick={() => play(12)}>
+                    play wave (12s)
+                </button>
             </div>
         </div>
     )
